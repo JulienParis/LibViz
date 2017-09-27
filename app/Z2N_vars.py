8063b28f6f1675ec9980ca70cb4ce0f5cf58aebe
Jpy <jparis.py@gmail.com>
2017-09-27 23:39:02 +0200

    added atelier magnan

--- app/Z2N_vars.py
+++ app/Z2N_vars.py
@@ -473,10 +473,10 @@ and organisation which produce conflicts with the controversial organisations an
                                 'credits'      : """...""",
                             },
             'dataSet_key'   : '1411268', ### aka group key
-            'url_ROOT'      : create_API_URL('1307836'),
-            'url_WEB'       : create_WEB_URL('1307836'),
+            'url_ROOT'      : create_API_URL('1411268'),
+            'url_WEB'       : create_WEB_URL('1411268'),
             'urlsDict'      : {
-
+                                '5QGTSJS5'  : {'name' : 'Nathalie Magnan' , 'len' : 0, 'hex' : '#325d58'},
                             },
             'nodesColorsDict' : {
                     node_str_dict['group']     : {'name' : 'red'  , 'hex' : '#8eba7d' }, ###############
